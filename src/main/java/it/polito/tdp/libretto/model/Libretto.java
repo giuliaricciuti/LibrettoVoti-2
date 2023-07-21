f893a1154fbe3659149fd122bc04f05693d24c24
giuliaricciuti <giuliaricciuti@localhost>
2023-07-21 14:02:12 +0200

    com1

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,84 @@
 package it.polito.tdp.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
 
+	private List<Voto> voti;
+	
+
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+		
+	
+	public List<Voto> getVoti() {
+		return voti;
+	}
+
+
+	public void setVoti(List<Voto> voti) {
+		this.voti = voti;
+	}
+
+	public boolean esisteVotoDupicato (Voto v) {
+		for (Voto v1: this.voti) {
+			if (v.isDuplicato(v1))
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean esisteConflitto (Voto v) {
+		for (Voto v1: this.voti) {
+			if (v.isConflitto(v1))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean add(Voto v) {
+		for (Voto v1: this.voti) {
+			if (v.isDuplicato(v1) || v.isConflitto(v1))
+				return false;
+		}
+		 voti.add(v);
+		 return true;
+	}
+	
+	public Voto cercaCorso25 () {
+		for (Voto v: voti) {
+			if (v.getPunti()==25)
+				return v;
+		}
+		return null;
+	}
+	
+	public Voto cercaPerNome(String nome) {
+		for(Voto v: voti) {
+			if (v.getCorso().equals(nome))
+				return v;
+		}
+		return null;
+	}
+	
+	public List miglioraLibretto(Libretto l) {	
+		List <Voto> depurati = new ArrayList<Voto>();;
+		for (Voto v: l.getVoti()) {
+			Voto v2 = v;
+			if ((v2.getPunti()>=18 && v2.getPunti()<24) || v2.getPunti()==29) {
+				v2.setPunti(v2.getPunti()+1);
+				depurati.add(v2);
+			}
+			else if (v2.getPunti()>=24 && v2.getPunti()<29) {
+				v2.setPunti(v2.getPunti()+1);
+				depurati.add(v2);
+			}
+		}
+		return depurati;
+	}
+	
+
+	
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,10 +1,42 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Libretto lib = new Libretto();
+		Libretto dep = new Libretto();
+		
+		lib.add(new Voto ("Analisi 1", 29, LocalDate.of(2021, 2, 15)));
+		lib.add(new Voto ("Fisica 2", 21, LocalDate.of(2022, 4, 7)));
+		lib.add(new Voto ("Informatica", 25, LocalDate.of(2023, 5, 4)));
+		
+		
+		Voto v = lib.cercaCorso25();
+		System.out.println(v.toString());
+		
+		v = lib.cercaPerNome("Analisi 1");
+		System.out.println(v.toString()+"\n");
+		
+		Voto v1 = new Voto ("Analisi 1", 29, LocalDate.of(2021, 2, 15));
+		System.out.println(lib.esisteVotoDupicato(v1));
+		
+		Voto v2 = new Voto ("Analisi 1", 18, LocalDate.of(2021, 2, 15));
+		System.out.println(lib.esisteConflitto(v2));
+		
+		lib.add(v1);
+		lib.add(v2);
+		
+		for (Voto v3 : lib.getVoti()) {
+			System.out.println (v3.toString());
+		}
+		
+		dep.miglioraLibretto(lib);
+		for (Voto v4 : dep.getVoti()) {
+			System.out.println (v4.toString());
+		}
 	}
 
 }
